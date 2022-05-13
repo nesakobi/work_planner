ba6f54b613d9016602c37bde521bdc1723c69326
nesakobi <hovhannes.hakobyan.1995@gmail.com>
2022-05-13 10:19:12 +0400

    Project structure and some models.

---
+++ src/Client/Desktop/Windows/Desktop/WorkPlanning.Core.UI/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MugenMvvmToolkit.WPF.Binding;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WorkPlanning.Core.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        public App()
+        {
+            WpfDataBindingExtensions.InitializeFromDesignContext();
+        }
+
+        protected override Window CreateShell()
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+        }
+    }
+}
---
+++ src/Client/Desktop/Windows/Desktop/WorkPlanning.UI.Views/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkPlanning.UI.Views
+{
+    public class Class1
+    {
+    }
+}
